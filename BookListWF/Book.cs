15ac6b48ba618b9087aa2d508391d3fb99d3a634
Jakub Strawa <01143607@pw.edu.pl>
2020-11-12 12:45:58 +0100

    Added Book.cs atributes

--- BookListWF/Book.cs
+++ BookListWF/Book.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace BookListWF
 {
-    class Book
+    public class Book
     {
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string Author
+        {
+            get;
+            set;
+        }
+
+        public DateTime ReleaseDate
+        {
+            get;
+            set;
+        }
+
+        public int Genre
+        {
+            get;
+            set;
+        }
+
+        public Book( string title, string author, DateTime releaseDate, int genre )
+        {
+            Title = title;
+            Author = author;
+            ReleaseDate = releaseDate;
+            Genre = genre;
+        }
     }
 }
